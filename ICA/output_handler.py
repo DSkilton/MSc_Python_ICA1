c2d2f9c01b74348e449555f92cf3edc41bab478c
DSkilton <duncanskilton.ds@gmail.com>
2024-12-03 18:45:48 +0000

    added docstring to output_handler

--- ICA/output_handler.py
+++ ICA/output_handler.py
@@ -1,6 +1,9 @@
-# TODO: Add doc string at class level and method level
 """
 This module will handle the formatting of any outputs
+
+Methods: 
+    format_two_decimals(number):
+        formats the passed number to 2 decimal places
 """
 
 class OutputHandler:
@@ -12,3 +15,4 @@ class OutputHandler:
     @staticmethod
     def format_two_decimals(number):
         return {f"{number:.2f}"}
+    
